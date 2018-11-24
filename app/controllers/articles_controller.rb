8747104e4642a97be2f0661c75f6540a61dd8095
Gregory Kallai <gerg.kallai@gmail.com>
2018-11-24 12:39:55 +0100

    adds method to saving data in controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,15 @@ class ArticlesController < ApplicationController
 
 
 def create
-  render plain: params[:article].inspect
+  @article = Article.new(article_params)
+
+  @article.save
+  redirect_to @article
 end
+
+private
+  def article_params
+      params.require(:article).permit(:title, :content)
+  end
 
 end

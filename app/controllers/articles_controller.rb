0f6980ebb107d78a34abd6cfd19c12637160128f
Gregory Kallai <gerg.kallai@gmail.com>
2018-11-24 12:59:00 +0100

    adds show link to show seperate article in the index page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,21 +7,21 @@ class ArticlesController < ApplicationController
   end
 
 
-def create
-  @article = Article.new(article_params)
+  def create
+    @article = Article.new(article_params)
 
-  @article.save
-  redirect_to @article
-end
-
-def show
-  @article = Article.find(params[:id])
-end
-
-
-private
-  def article_params
-      params.require(:article).permit(:title, :content)
+    @article.save
+    redirect_to @article
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+
+  private
+    def article_params
+        params.require(:article).permit(:title, :content)
+    end
 
 end
